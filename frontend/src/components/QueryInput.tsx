cbc3e9e8cca5532a24143ac0058126afcf70be51
Alex Figueroa <alexfig221@gmail.com>
2024-08-30 11:23:33 -0700

    Add label and placeholder to query input

--- frontend/src/components/QueryInput.tsx
+++ frontend/src/components/QueryInput.tsx
@@ -1,18 +1,26 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { useState } from 'react'
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
 
 function QueryInput({ onSearch }: { onSearch: (query: string) => void }) {
-  const [queryString, setQueryString] = useState<string>('')
+  const [queryString, setQueryString] = useState<string>('');
 
   return (
-    <div className="flex w-full items-center space-x-2 mb-5">
-      <Input type="text" onChange={e => setQueryString(e.target.value)} value={queryString} />
+    <div className="flex w-full items-end space-x-2 mb-5">
+      <div className="flex-1">
+        <label className="block">Query</label>
+        <Input
+          type="text"
+          onChange={e => setQueryString(e.target.value)}
+          value={queryString}
+          placeholder='ex: [["condition", "=", "P"], ["year_built", "<", "1950"]]'
+        />
+      </div>
       <Button type="submit" onClick={() => onSearch(queryString)}>
         Search
       </Button>
     </div>
-  )
+  );
 }
 
-export default QueryInput
+export default QueryInput;
